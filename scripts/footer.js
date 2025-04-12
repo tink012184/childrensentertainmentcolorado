a7bd36ae06fb9d6fd76e88c224bafb7eceff4907
Tink <85289337+tink012184@users.noreply.github.com>
2025-04-11 20:56:47 -0500

    approved by Dominick 4/11/2025

---
+++ scripts/footer.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const header = document.createElement("footer");
+  header.innerHTML = `        <div class="info-bar">
+          <span class="left">Dominick Black</span>
+          <span class="center">All Entertainment Colorado</span>
+          <a class="right" href="tel:7192330106"> 719.233.0106</a>
+        </div>`;
+  document.body.append(header);
+});
---
+++ scripts/header.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const header = document.createElement("header");
+  header.innerHTML = `
+         <div class="header-container">
+        <div class="header-logo">
+          <a href="https://allentertainment.com">
+            <img
+              src="images/LogoCENoBackground.png"
+              alt="All Entertainment Colorado"
+          /></a>
+        </div>
+        <nav class="header-nav">
+          <a href="https://childrensentertainmentcolorado.com/"
+            >Children's Entertainment Colorado</a
+          >
+          <a href="https://bestsoundcolorado.com/">Best Sound Colorado</a>
+          <a href="https://allentertainment.com/about">About All Entertainment Colorado</a>
+        </nav>
+      </div>
+  `;
+  document.body.prepend(header);
+});
